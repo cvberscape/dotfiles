cc305497a7b84e02a2dcfafc51b66eefa0342590
cvberscape <cvberscape@gmail.com>
2025-11-08 01:00:56 +0200

    updates

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "cvberscape";
+  home.homeDirectory = "/home/cvberscape";
+
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    android-studio
+    brave 
+    bun 
+    watchman
+    vesktop
+    heroic
+    catppuccin-cursors.mochaSky
+    #code-cursor-fhs
+    code-cursor
+    claude-code
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+}
